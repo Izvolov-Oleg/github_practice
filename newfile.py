938e55e1d8bc222ac2f0c81b28db6223e01c3943
Oleg Izvolov <oleg-izvolov@yandex.ru>
2020-11-27 23:27:41 +0400

    V4.0

--- newfile.py
+++ newfile.py
@@ -1,5 +1,6 @@
 # Создаем класс "робот" - решение прошло!
 print('Version 2.0')
+print('Version 3.0') # Del class Vector
 class Robot(object):
     population = 0
 
@@ -19,17 +20,4 @@ class Robot(object):
     def how_many():
         print(f'{Robot.population}, вот сколько нас еще осталось')
 
-class Vector:
 
-    def __init__(self, *args):
-        self.values = []
-        for arg in args:
-            if isinstance(arg, int):
-                self.values.append(arg)
-
-    def __str__(self):
-        if not self.values:
-            return f'Пустой вектор'
-        self.values.sort()
-        self.values = tuple(self.values)
-        return f'Вектор{self.values}'
