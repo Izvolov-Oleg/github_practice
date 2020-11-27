feb57b713cdeac134a73ac1aee88a7dbbceb7f61
Oleg Izvolov <oleg-izvolov@yandex.ru>
2020-11-27 23:38:04 +0400

    V2.1

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 
 from My_project import newfile as nf
 
-print('Version - V2.0')
+print('Version - V2.1')
 
 R1 = nf.Robot('A234')
 R1.say_hello()
 print(R1.name)
-V1 = nf.Vector(1, 2, 3)
-print(V1.values)
+
--- newfile.py
+++ newfile.py
@@ -1,5 +1,5 @@
 # Создаем класс "робот" - решение прошло!
-print('Version 2.0')
+print('Version 2.1')
 class Robot(object):
     population = 0
 
@@ -19,17 +19,17 @@ class Robot(object):
     def how_many():
         print(f'{Robot.population}, вот сколько нас еще осталось')
 
-class Vector:
-
-    def __init__(self, *args):
-        self.values = []
-        for arg in args:
-            if isinstance(arg, int):
-                self.values.append(arg)
-
-    def __str__(self):
-        if not self.values:
-            return f'Пустой вектор'
-        self.values.sort()
-        self.values = tuple(self.values)
-        return f'Вектор{self.values}'
+# class Vector:
+#
+#     def __init__(self, *args):
+#         self.values = []
+#         for arg in args:
+#             if isinstance(arg, int):
+#                 self.values.append(arg)
+#
+#     def __str__(self):
+#         if not self.values:
+#             return f'Пустой вектор'
+#         self.values.sort()
+#         self.values = tuple(self.values)
+#         return f'Вектор{self.values}'
